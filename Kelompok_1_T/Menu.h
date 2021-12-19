4aa5820633ee797bda2f7cefb37f01cbc385701b
students124 <billyyuriaan@students.telkomuniversity.ac.id>
2021-12-19 14:34:52 +0700

    added

--- Kelompok_1_T/Menu.h
+++ Kelompok_1_T/Menu.h
@@ -10,6 +10,6 @@ using namespace std;
 
 int getOption_1303204141();
 void showPengerajinx_1303204141(List_pengerajin L, string nama, int umur);
-void deletepengerajinTertentu_1303204141(List_pengerajin L, string nama, int umut);
+void deletepengerajinTertentu_1303204141(List_pengerajin L, string nama, int umur);
 
 #endif // MENU_H_INCLUDED
